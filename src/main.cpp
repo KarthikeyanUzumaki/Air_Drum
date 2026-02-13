abd37877d708bbcdb1547c1d26fc7b11dac0d43c
KarthikeyanUzumaki <karthikeyan245242@gmail.com>
2026-02-13 15:52:07 +0530

    Refactor code structure for improved readability and maintainability of Air-Drum

--- src/main.cpp
+++ src/main.cpp
@@ -11,63 +11,66 @@ Adafruit_MPU6050 mpu;
 BLECharacteristic *pCharacteristic;
 bool deviceConnected = false;
 
-// Unique IDs for Bluetooth
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
-float lastAccelZ = 0;
+float yaw = 0;
+unsigned long lastTime = 0;
 unsigned long lastHitTime = 0;
-const int debounceDelay = 150; 
-const float jerkThreshold = 15.0; // Adjust this for sensitivity
 
 class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) { deviceConnected = true; }
     void onDisconnect(BLEServer* pServer) { 
         deviceConnected = false;
-        pServer->getAdvertising()->start(); // Restart advertising
+        pServer->getAdvertising()->start(); 
     }
 };
 
 void setup() {
     Serial.begin(115200);
-    
-    // Initialize MPU6050
-    if (!mpu.begin()) {
-        Serial.println("Failed to find MPU6050 chip");
-        while (1) yield();
-    }
-    Wire.setClock(400000); // Fast I2C
+    if (!mpu.begin()) { while (1) yield(); }
+    Wire.setClock(400000); 
 
-    // Setup Bluetooth
     BLEDevice::init("PocketDrum-DIY");
     BLEServer *pServer = BLEDevice::createServer();
     pServer->setCallbacks(new MyServerCallbacks());
     BLEService *pService = pServer->createService(SERVICE_UUID);
-    pCharacteristic = pService->createCharacteristic(
-                        CHARACTERISTIC_UUID,
-                        BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY
-                      );
+    pCharacteristic = pService->createCharacteristic(CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_NOTIFY);
     pCharacteristic->addDescriptor(new BLE2902());
     pService->start();
     pServer->getAdvertising()->start();
-    Serial.println("Waiting for a client connection...");
 }
 
 void loop() {
     sensors_event_t a, g, temp;
     mpu.getEvent(&a, &g, &temp);
 
-    float currentAccelZ = a.acceleration.z;
-    float jerk = abs(currentAccelZ - lastAccelZ);
-    lastAccelZ = currentAccelZ;
+    unsigned long currentTime = millis();
+    float dt = (currentTime - lastTime) / 1000.0;
+    lastTime = currentTime;
 
-    if (jerk > jerkThreshold && (millis() - lastHitTime) > debounceDelay) {
+   
+    if (abs(g.gyro.z) > 0.15) { 
+        yaw += g.gyro.z * dt * (180.0 / PI);
+    } else {
+        yaw *= 0.98; 
+    }
+
+    
+    if (digitalRead(0) == LOW) { yaw = 0; delay(300); }
+
+    float totalAccel = sqrt(sq(a.acceleration.x) + sq(a.acceleration.y) + sq(a.acceleration.z));
+
+    if (totalAccel > 28.0 && (millis() - lastHitTime) > 150) {
+        String hitType = "SN";
+        if (yaw < -35) hitType = "HH";     
+        else if (yaw > 0) hitType = "KK";  
+      
         if (deviceConnected) {
-            pCharacteristic->setValue("HIT");
+            pCharacteristic->setValue(hitType.c_str());
             pCharacteristic->notify();
-            Serial.println("Drum Hit Detected!");
         }
+        Serial.print("Zone: "); Serial.print(hitType); Serial.print(" | Yaw: "); Serial.println(yaw);
         lastHitTime = millis();
     }
-    delay(10); // 100Hz sampling rate
 }
